6695b9bfecc851a132e4e23f27317ef5703fbd30
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-01-09 16:57:36 +0100

    test_vertex, add test finding edge

--- src/test/app/graph/test_vertex.py
+++ src/test/app/graph/test_vertex.py
@@ -8,29 +8,42 @@ class TestVertex(unittest.TestCase):
     def setUp(self):
         self.vertex1 = Vertex('1', 120, 120)
         self.vertex2 = Vertex('2', 120, 120)
-        self.vertex3 = Vertex('3', 120, 120)
-        self.vertex4 = Vertex('2', 120, 120)
-        self.edge_directed1 = Edge(self.vertex1, self.vertex2, True, False, None)
-        self.edge_directed2 = Edge(self.vertex2, self.vertex1, True, False, None)
-        self.edge_directed3 = Edge(self.vertex1, self.vertex3, True, False, None)
-        self.edge_directed4 = Edge(self.vertex1, self.vertex2, True, False, None)
+        self.edge_directed_v1_v2 = Edge(self.vertex1, self.vertex2, True, False, None)
+        self.edge_directed_v2_v1 = Edge(self.vertex2, self.vertex1, True, False, None)
 
-        self.edge_undirected1 = Edge(self.vertex1, self.vertex2, False, False, None)
-        self.edge_undirected2 = Edge(self.vertex2, self.vertex1, False, False, None)
-        self.edge_undirected3 = Edge(self.vertex2, self.vertex3, False, False, None)
-        self.edge_undirected4 = Edge(self.vertex1, self.vertex2, True, False, None)
+        self.edge_undirected_v1_v2 = Edge(self.vertex1, self.vertex2, False, False, None)
+        self.edge_undirected_v2_v1 = Edge(self.vertex2, self.vertex1, False, False, None)
 
-    def test_add_neighbor(self):
-        self.vertex1.add_neighbor(self.vertex2, self.edge_undirected1)
-        self.vertex1.add_neighbor(self.vertex3, self.edge_undirected3)
-
-        self.assertEqual(len(self.vertex1.edges), 2)
-        self.assertEqual(len(self.vertex1.neighbors), 2)
-
-        self.assertEqual(self.vertex1.edges.__contains__(self.edge_undirected1), True)
-        self.assertEqual(self.vertex1.edges.__contains__(self.edge_undirected3), True)
+    def test_add_neighbor_directed_edge(self):
+        self.vertex1.add_neighbor(self.vertex2, self.edge_directed_v1_v2)
+        self.assertEqual(len(self.vertex1.neighbors), 1)
+        self.assertEqual(len(self.vertex2.neighbors), 0)
+        self.assertEqual(len(self.vertex1.edges), 1)
+        self.assertEqual(len(self.vertex2.edges), 0)
         self.assertEqual(self.vertex1.neighbors[0], self.vertex2)
-        self.assertEqual(self.vertex1.neighbors[1], self.vertex3)
 
-    def test_find_edge(self):
-        self.fail()
+    def test_add_neighbor_undirected_edge(self):
+        self.vertex1.add_neighbor(self.vertex2, self.edge_undirected_v1_v2)
+        self.vertex2.add_neighbor(self.vertex1, self.edge_undirected_v2_v1)
+        self.assertEqual(len(self.vertex1.neighbors), 1)
+        self.assertEqual(len(self.vertex2.neighbors), 1)
+        self.assertEqual(len(self.vertex1.edges), 1)
+        self.assertEqual(len(self.vertex2.edges), 1)
+        self.assertEqual(self.vertex1.neighbors[0], self.vertex2)
+        self.assertEqual(self.vertex2.neighbors[0], self.vertex1)
+
+    def test_find_edge_directed(self):
+        self.vertex1.add_neighbor(self.vertex2, self.edge_directed_v1_v2)
+        self.vertex2.add_neighbor(self.vertex1, self.edge_directed_v2_v1)
+
+        edge = self.vertex1.find_edge(self.vertex2, True)
+        self.assertEqual(edge, self.edge_directed_v1_v2)
+        self.assertNotEqual(edge, self.edge_directed_v2_v1)
+
+    def test_find_edge_undirected(self):
+        self.vertex1.add_neighbor(self.vertex2, self.edge_undirected_v1_v2)
+        self.vertex2.add_neighbor(self.vertex1, self.edge_undirected_v2_v1)
+
+        edge = self.vertex1.find_edge(self.vertex2, False)
+        self.assertEqual(edge, self.edge_undirected_v1_v2)
+        self.assertEqual(edge, self.edge_undirected_v2_v1)
