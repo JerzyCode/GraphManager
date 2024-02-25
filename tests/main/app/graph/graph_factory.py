1d1a5beb81f848b9a38b3998c564ac82771f13af
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-02-25 17:23:08 +0100

    add test edge_drawer

--- tests/main/app/graph/graph_factory.py
+++ tests/main/app/graph/graph_factory.py
@@ -1,5 +1,6 @@
 import random as rd
 
+from src.main.app.graph.digraph import Digraph
 from src.main.app.graph.directed_graph import DirectedGraph
 from src.main.app.graph.edge import Edge
 from src.main.app.graph.graph import Graph
@@ -80,6 +81,19 @@ def generate_test_two_vertex_directed_graph():
     max_width = rd.randint(150, 500)
     max_height = rd.randint(150, 500)
     graph = DirectedGraph([], is_weighted=False, max_width=max_width, max_height=max_height)
+    vertex1 = generate_test_vertex("1")
+    vertex2 = generate_test_vertex("2")
+    edge = Edge(vertex1, vertex2, directed=True, digraph=False, weight=None)
+    graph.E.add(edge)
+    graph.V.append(vertex1)
+    graph.V.append(vertex2)
+    return graph
+
+
+def generate_test_two_vertex_digraph():
+    max_width = rd.randint(150, 500)
+    max_height = rd.randint(150, 500)
+    graph = Digraph([], is_weighted=False, max_width=max_width, max_height=max_height)
     vertex1 = generate_test_vertex("1")
     vertex2 = generate_test_vertex("2")
     edge = Edge(vertex1, vertex2, directed=True, digraph=False, weight=None)
